a5e1337c5acb222aaa3218b0c3966b9c4f6ee0eb
Junior Campos <junior.campos@welkomtravel.com>
2019-11-29 18:32:41 -0500

    added ver

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -12,11 +12,22 @@ import BlogSection from '../components/BlogSection';
 const Landing = () => {
   return (
     <TemplateWrapper2>
-        <section className="jumbotron jumbotron-fluid p-0 m-0">
+        <section
+          id="jumbo" 
+          className="jumbotron jumbotron-fluid p-0 m-0 position-relative">
+          <img 
+              // style={{zIndex: "-100"}}
+              className="jumbo__cover w-100 h-100"
+              src="/img/cover.svg"
+              alt="cover" />
+            {/* <div
+              className="jumbo__desc text-white font-weight-bold text-uppercase position-absolute">
+              Logra tu mejor version
+            </div>
             <img 
-                className="img-fluid"
-                src="/img/cover.png"
-                alt="cover" />
+                className="jumbo__logo position-absolute"
+                src="/img/logo.svg"
+                alt="logo" /> */}
         </section>
 
         {/* Info doctor */}
